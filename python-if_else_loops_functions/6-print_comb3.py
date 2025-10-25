10df26c16aaa17bb97ad2b9e29e0106e0f09b8d9
natahaciyeva <natiagadjieva@gmail.com>
2025-10-25 17:55:17 +0000

    6-print_comb3.py

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if j > i:
+            if i != 8 or j != 9:
+                print("{}{}".format(i, j), end=", ")
+            else:
+                print("{}{}".format(i, j))
