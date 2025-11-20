057db2c19e96914e85d56b97b5fb97cfee2ad3a1
natahaciyeva <natiagadieva@gmail.com>
2025-11-20 17:31:43 +0000

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with optional JSON serialization."""
+
+
+class Student:
+    """Defines a student with first_name, last_name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation of the Student instance.
+        If attrs is a list of strings, only those attributes are included.
+        """
+        if isinstance(attrs, list):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()
