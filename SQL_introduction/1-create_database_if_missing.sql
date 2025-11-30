e9a6bc7ae33d978852cfde031cafbc914bf30172
natahaciyeva <natiagadjieva@gmail.com>
2025-11-30 18:27:54 +0000

    1-create_database_if_missing.sql

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- 1-create_database_if_missing.sql
+-- Bu skript hbtn_0c_0 adlı verilənlər bazasını yaradır
+-- Əgər baza artıq varsa, heç bir xəta verməyəcək
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
