ee83a14ca277476ecee7c3cc2b2dfdd9012d7464
natahaciyeva <natiagadjieva@gmail.com>
2025-11-30 18:36:37 +0000

    3-list_tables.sql

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- 3-list_tables.sql
+-- Bu skript verilənlər bazasındakı bütün cədvəlləri sadalayır
+-- Database adı mysql command ilə veriləcək (məsələn: mysql -hlocalhost -uroot -p <database_name>)
+SHOW TABLES;
