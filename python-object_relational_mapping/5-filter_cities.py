b7c35484296be447469f4083bbb1456728f16261
root <natiagadjieva@gmail.com>
2025-12-03 19:24:05 +0000

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,55 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument and lists all
-cities of that state, using the database hbtn_0e_4_usa.
+Lists all cities of a state from the database hbtn_0e_4_usa
 """
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states "
-                "ON cities.state_id = states.id WHERE states.name = %s "
-                "ORDER BY cities.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
-    cur.close()
+
+def main():
+    if len(sys.argv) != 5:
+        return
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    # SQL injection free query using placeholders
+    query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC;
+    """
+
+    cursor.execute(query, (state_name,))
+
+    rows = cursor.fetchall()
+
+    # Extract city names into a list
+    city_names = [row[0] for row in rows]
+
+    # Print in required format
+    print(", ".join(city_names))
+
+    cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
+
